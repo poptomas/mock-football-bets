65df9878dad6da979ca6aa0e8f458ba9c159961b
todayisntmonday <60581440+todayisntmonday@users.noreply.github.com>
2021-06-06 11:13:33 +0200

    Update Home.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,11 +2,7 @@ import React, {useState, useEffect} from 'react';
 
 export const Home = () => (
     <div>
-<<<<<<< HEAD
-      <h1>This is the Home Page</h1>
-=======
       <h1>Mock Football Bets - Homepage</h1>
->>>>>>> api-flask-react
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras gravida,
         risus at dapibus aliquet, elit quam scelerisque tortor, nec accumsan eros
