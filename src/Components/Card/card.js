e022c6fb0e9eec6c4b692a20585158e400ef824d
todayisntmonday <60581440+todayisntmonday@users.noreply.github.com>
2021-06-06 11:14:37 +0200

    Update card.js

--- src/Components/Card/card.js
+++ src/Components/Card/card.js
@@ -1,15 +1,10 @@
 import React from 'react';
 
 export const Card = () => {
-<<<<<<< HEAD
-    return <div>Hello</div>
-=======
-
     return (
         <div>
         <h1> Mock Football Bets </h1>
         <h2> {new Date().toDateString()} </h2>
         </div>
     )
->>>>>>> api-flask-react
 };
