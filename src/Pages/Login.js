624a90ad3eaf266d604032931f308e60a6acd322
todayisntmonday <60581440+todayisntmonday@users.noreply.github.com>
2021-06-06 11:13:09 +0200

    Update Login.js

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,28 +1,8 @@
 import React, {useState, useEffect} from 'react';
 
-<<<<<<< HEAD
-import {Card} from '../Components/Card/card';
-
 import {Form} from '../Components/Forms/RegistrationForm';
-
-export const Login = () => {
-    
-    const [todo, setTodo] = useState([]);
-
-    useEffect( () => {
-        fetch("/api/login").then(response => {
-            if(response.ok)
-                return response.json()
-        }).then(data => console.log(data))
-    }, [])
-    
-    
-    return(
-        <>
-        <Card />
-=======
-import { Card } from '../Components/Card/card';
 import { LoginForm } from '../Components/Forms/LoginForm';
+import { Card } from '../Components/Card/card';
 
 export const Login = () => {
 
@@ -43,7 +23,6 @@ export const Login = () => {
         <>
         <Card />
         <LoginForm />
->>>>>>> api-flask-react
         </>
     )
 }
